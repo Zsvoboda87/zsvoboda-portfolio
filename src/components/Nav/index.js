6818a26bdf8e36e8f990145d1c08db93552b72f2
Zsvob <svobodazachary@gmail.com>
2022-04-08 11:00:17 -0400

    nav start

---
+++ src/components/Nav/index.js
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#about-me">About Me</a></li>
+                <li><a href="#previous-work">Previous Work</a></li>
+                <li><a href="#contact-me">Contact Me</a></li>
+                <li><a href="">Resume</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+
+export default Nav;
