8322fde73921226e65f814493c51658cd42495ee
Zsvob <svobodazachary@gmail.com>
2022-04-08 14:41:54 -0400

    refactor Nav

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import './App.css';
-import Nav from './components/Nav';
+import React, { useState, useEffect } from "react";
+import NavTabs from './components/Nav/NavTabs';
+import Portfolio from './components/pages/Portfolio';
+import Contact from './components/pages/Contact';
+import Resume from './components/pages/Resume';
+import About from './components/pages/About';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const renderPage = () => {
+    if (currentPage === 'Portfolio') {
+      return <Portfolio />;
+    }
+    if (currentPage === 'Contact') {
+      return <Contact />;
+    }
+    if (currentPage === 'Resume') {
+      return <Resume />;
+    }
+    return <About />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+
   return (
     <div className="App">
       <header>
         <h1>Zachary Svoboda</h1>
-        <Nav></Nav>
+        <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
       </header>
 
       <main>
-
-
+        {renderPage()}
       </main>
     </div>
   );
