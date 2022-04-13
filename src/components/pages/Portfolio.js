3455b5eb256ce44f70d975eb55bf0fe9b4de25c0
Zsvob <svobodazachary@gmail.com>
2022-04-13 14:33:32 -0400

    change github link location

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -4,42 +4,36 @@ export default function Portfolio() {
     return (
         <div className='flex-evenly'>
             <a className='nav-link vw43' href="https://mysterious-eyrie-75602.herokuapp.com/" id="puppy-love">
-                <h3>Puppy-Love</h3>
+                <h3>Puppy-Love <a className='nav-link' href='https://github.com/Zsvoboda87/Puppy-Love'>Github</a></h3>
                 <h5>Express JS, MySql2, Cloudinary, Sequelize</h5>
-                <a className='nav-link' href='https://github.com/Zsvoboda87/Puppy-Love'>Github</a>
                 <img className="vw43" src="assets/images/puppylove.jpg" alt="Puppy Love Webpage cover"></img>
             </a>
             <a className='nav-link vw43' href="https://zsvoboda87.github.io/Bored-AF/">
-                <h3>Bored-AF</h3>
+                <h3>Bored-AF <a className='nav-link' href='https://github.com/Zsvoboda87/Bored-AF'>Github</a></h3>
                 <h5>HTML, CSS, JavaScript, Third Party Apis</h5>
-                <a className='nav-link' href='https://github.com/Zsvoboda87/Bored-AF'>Github</a>
                 <img className="vw43" src="assets/images/Bored-AF.png" alt="Bored A F Webpage cover"></img>
             </a>
             <a className='nav-link vw43' href="https://watch.screencastify.com/v/2JGX16nUDU8GSKy7FYw2">
-                <h3>SQl Employee Tracker</h3>
+                <h3>SQl Employee Tracker <a className='nav-link' href='https://github.com/Zsvoboda87/SQL-Employee-Tracker'>Github</a></h3>
                 <h5>MySql, Node JS, inquierer</h5>
-                <a className='nav-link' href='https://github.com/Zsvoboda87/SQL-Employee-Tracker'>Github</a>
                 <img className="vw43" src="assets/images/SQL-employeeTracker.jpg" alt="mySql Employee Tracker"></img>
             </a>
             <a className='nav-link vw43' href="https://young-temple-73421.herokuapp.com/">
-                <h3>The Nerd Spot</h3>
+                <h3>The Nerd Spot <a className='nav-link' href='https://github.com/Zsvoboda87/The-Nerd-Spot'>Github</a></h3>
                 <h5>MySql, HandleBars, Express</h5>
-                <a className='nav-link' href='https://github.com/Zsvoboda87/The-Nerd-Spot'>Github</a>
                 <img className="vw43" src="assets/images/The-Nerd-Spot.png" alt="The nerd Spot Cover"></img>
             </a>
             <a className='nav-link vw43' href="https://stark-badlands-84312.herokuapp.com/">
-                <h3>Pizza Hunt</h3>
+                <h3>Pizza Hunt <a className='nav-link' href='https://github.com/Zsvoboda87/pizza-hunt'>Github</a></h3>
                 <h5>MongoDb, Mongoose</h5>
-                <a className='nav-link' href='https://github.com/Zsvoboda87/pizza-hunt'>Github</a>
                 <img className="vw43" src="assets/images/Pizza-Hunt.png" alt="Pizza-Hunt Cover"></img>
             </a>
             <a className='nav-link vw43' href="https://zsvoboda87.github.io/Portfolio/">
-                <h3>Portfolio 1</h3>
+                <h3>Portfolio 1 <a className='nav-link' href='https://github.com/Zsvoboda87/Portfolio'>Github</a></h3>
                 <h5>HTML, CSS</h5>
-                <a className='nav-link' href='https://github.com/Zsvoboda87/Portfolio'>Github</a>
                 <img className="vw43" src="assets/images/Portfolio1.png" alt="Zachary's First Porfolio"></img>
-            </a>
+            </a >
 
-        </div>
+        </div >
     );
 }
