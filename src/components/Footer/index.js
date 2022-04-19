85a6ca5f01dfaa612fddf52a9e1374330db867a0
Zsvob <svobodazachary@gmail.com>
2022-04-19 10:46:46 -0400

    footer links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,8 +5,8 @@ import React from 'react';
 function Footer() {
     return (
         <div className="flex-evenly">
-            <a className='footer-link' href=""> Github </a>
-            <a className='footer-link' href=""> Linked in </a>
+            <a className='footer-link' href="https://github.com/Zsvoboda87"> Github </a>
+            <a className='footer-link' href="https://www.linkedin.com/in/zachary-svoboda-3a0481225/"> Linked in </a>
             <a className='footer-link' href="mailto:svobodazachary@gmail.com"> Email </a>
         </div>
     );
