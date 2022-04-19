0f05381bac5359356f23ab0836567694b21f4af2
Zsvob <svobodazachary@gmail.com>
2022-04-19 10:23:57 -0400

    resume

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Resume() {
     return (
         <div className='flex-evenly'>
-            <a href="../../../assets/ZsvobodaResume.pdf"><h1>Click to Here to Download Resume</h1></a>
+            <a href="assets/ZsvobodaResume.pdf"><h1>Click to Here to Download Resume</h1></a>
         </div>
     );
 }
